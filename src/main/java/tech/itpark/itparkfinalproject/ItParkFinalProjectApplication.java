17f697cab7312821c32d67d4a80fa79c1141d560
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-05-12 23:22:33 +0300

    Убрал тестовый код

--- src/main/java/tech/itpark/itparkfinalproject/ItParkFinalProjectApplication.java
+++ src/main/java/tech/itpark/itparkfinalproject/ItParkFinalProjectApplication.java
@@ -4,9 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import tech.itpark.itparkfinalproject.service.CategoryService;
-import tech.itpark.itparkfinalproject.service.ProductService;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -14,14 +11,7 @@ import tech.itpark.itparkfinalproject.service.ProductService;
 public class ItParkFinalProjectApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(ItParkFinalProjectApplication.class, args);
-        CategoryService service = run.getBean(CategoryService.class);
-        ProductService productService = run.getBean(ProductService.class);
-
-        /*System.out.println(service.findAll());
-        System.out.println("--------------------------");
-        System.out.println(service.findById("9f1d52ad-21b2-4736-9945-b11309e72268"));*/
-
+      SpringApplication.run(ItParkFinalProjectApplication.class, args);
     }
 
 }
