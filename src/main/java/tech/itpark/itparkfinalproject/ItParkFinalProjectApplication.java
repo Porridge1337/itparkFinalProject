9ee06e8d21c4eedc80075efbf44c0d36138ed10a
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-05-20 01:18:40 +0300

    Убрал ненужные зависимости в классе запускаторе

--- src/main/java/tech/itpark/itparkfinalproject/ItParkFinalProjectApplication.java
+++ src/main/java/tech/itpark/itparkfinalproject/ItParkFinalProjectApplication.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @RequiredArgsConstructor
